9c340995f91edf57e34a33413af1eb1fdb0f8f6f
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-03-21 07:14:37 +0100

    Added ReadLines()

---
+++ src/WouterVanRanst.Utils/Extensions/TextReaderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WouterVanRanst.Utils.Extensions;
+
+public static class TextReaderExtensions
+{
+    public static IEnumerable<string> ReadLines(this TextReader reader)
+    {
+        // https://stackoverflow.com/a/28365736/1582323
+        while (reader.ReadLine() is { } line)
+        {
+            yield return line;
+        }
+    }
+}
