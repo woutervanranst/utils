27c7885f8e00b3dee0d6bc6a5938145ed55f8fe0
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-07-31 09:05:12 +0200

    Create IGroupingExtensions.cs

---
+++ src/WouterVanRanst.Utils/Extensions/IGroupingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WouterVanRanst.Utils.Extensions;
+
+public static class IGroupingExtensions
+{
+    public static IEnumerable<TElement> GetGroup<TKey, TElement>(this IEnumerable<IGrouping<TKey, TElement>> groupings, TKey groupId)
+    {
+        var group = groupings.SingleOrDefault(g => g.Key.Equals(groupId));
+        return group?.AsEnumerable() ?? Enumerable.Empty<TElement>();
+    }
+}
