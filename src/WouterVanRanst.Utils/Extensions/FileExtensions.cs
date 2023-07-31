8bb4faa62eea83986656d957b987d3fa779aa80b
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-07-31 14:30:22 +0200

    chores

--- src/WouterVanRanst.Utils/Extensions/FileExtensions.cs
+++ src/WouterVanRanst.Utils/Extensions/FileExtensions.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace WouterVanRanst.Utils.Extensions;
 
-namespace WouterVanRanst.Utils.Extensions
+public static class FileExtensions
 {
-    public static class FileExtensions
+    /// <summary>
+    /// Return the length of a file in bytes
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static long Length(this string path)
     {
-        /// <summary>
-        /// Return the length of a file in bytes
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        public static long Length(this string path)
-        {
-            // Return the length of a file, without using FileInfo and with minimal allocations
-            using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
-            return fileStream.Length;
-        }
+        // Return the length of a file, without using FileInfo and with minimal allocations
+        using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        return fileStream.Length;
     }
 }
