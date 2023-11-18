8dec1cb6342772e7183c4b128b14c0de47f20bba
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-11-18 13:57:53 +0100

    Create SequenceDiagramBuilder.cs

---
+++ src/WouterVanRanst.Utils/Builders/SequenceDiagramBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection.Emit;
+using System.Text;
+
+namespace WouterVanRanst.Utils.Builders
+{
+    public class SequenceDiagramBuilder
+    {
+        public string? Title { get; set; }
+
+        private readonly Dictionary<string, string> participants = new();
+        private readonly List<(string from, string to, string label, bool async)> steps = new();
+
+        public void AddParticipant(string key, string caption)
+        {
+            participants.Add(key, caption);
+        }
+
+        public void AddStep(string from, string to, string label, bool async = false)
+        {
+            steps.Add((from, to, label, async));
+        }
+        
+        public override string ToString()
+        {
+            var seq = new StringBuilder();
+
+            //seq.AppendLine("sequenceDiagram");
+            
+            if (Title is not null)
+                seq.AppendLine($"title {Title}");
+
+            seq.AppendLine();
+
+            foreach (var participant in participants)
+                seq.AppendLine($"participant \"{participant.Value}\" as {participant.Key}");
+
+            seq.AppendLine();
+
+            foreach (var step in steps)
+            {
+                if (step.async)
+                    seq.AppendLine($"{step.from} ->(2) {step.to}: {step.label}");
+                else
+                    seq.AppendLine($"{step.from} -> {step.to}: {step.label}");
+            }
+
+            return seq.ToString();
+        }
+    }
+}
