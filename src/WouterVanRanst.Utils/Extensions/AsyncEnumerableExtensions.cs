7c3c24634a66454e81675e678effa8b8455b4c36
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-08-28 20:57:30 +0200

    added AsyncEnumerableExtensions.ToListAsync

---
+++ src/WouterVanRanst.Utils/Extensions/AsyncEnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace WouterVanRanst.Utils.Extensions;
+
+public static class AsyncEnumerableExtensions
+{
+    public static async Task<List<T>> ToListAsync<T>(this IEnumerable<Task<T>> source)
+    {
+        var results = await Task.WhenAll(source);
+        return results.ToList();
+    }
+}
